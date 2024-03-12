a13d472924bd4fb01d0619027df00a8eb3b21919
dejanilic <ilicdejan1997@gmail.com>
2024-03-12 18:29:16 +0100

    Initial commit

---
+++ src/test/java/com/heliant/formservice/FormServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heliant.formservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
